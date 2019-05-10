938ef9c723d0d64630ee0be710973c1da5cc9855
Seth Mitchell <47039952+LofiWiz@users.noreply.github.com>
2019-05-09 19:25:42 -0600

    Add files via upload

---
+++ exercise2.3.py
@@ -0,0 +1,9 @@
+# Seth Mitchell (10600367)
+# CS 1400
+# Exercise 2.3
+
+# 3: printing out squres of integers 1 - 5
+
+# variables
+squares_of_integers = [x ** 2 for x in range(1, 6)]
+print(squares_of_integers)
